969ebd7f306e28d79bad89bd337fa916b567f8b4
phoenix-ZY <hth_2003@sjtu.edu.cn>
2023-12-21 22:10:27 +0800

    update divide

--- TFIDF.py
+++ TFIDF.py
@@ -1,8 +1,9 @@
 from PreProgress import TFIDF_process
 from Cluster import kmeans
-
+from PreProgress.utils import divide_csv
 
 filename = "CrawlingStuff\CrawlResult\WEIBO_#张雪峰回应文科都是服务业#.csv"
 word_outputs = TFIDF_process(filename)
 lables = kmeans(word_outputs)
 print(lables.sum())
+divide_csv(filename,lables,model_name = "TFIDF")
